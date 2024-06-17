8cfb03616cc4d672662bb696aa2c393074013295
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-06-17 18:58:22 +0800

    Update MessageHandler.java

--- messagebrz/src/main/java/bbcdabao/componentsbrz/messagebrz/api/annotation/MessageHandler.java
+++ messagebrz/src/main/java/bbcdabao/componentsbrz/messagebrz/api/annotation/MessageHandler.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package bbcdabao.componentsbrz.messagebrz.api.annotation;
 
 import static java.lang.annotation.ElementType.TYPE;
@@ -11,9 +29,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * -用于类上，标注处理模块
- * @author bao
- *
+ * Used on messageHandler
  */
 @Target(TYPE)
 @Retention(RUNTIME)
