bb9de529aea1ea6288e5bba5aeebe94556f67ba0
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-06-25 17:20:49 +0800

    Update SessionSender.java

--- websocketbrz/src/main/java/bbcdabao/componentsbrz/websocketbrz/impl/SessionSender.java
+++ websocketbrz/src/main/java/bbcdabao/componentsbrz/websocketbrz/impl/SessionSender.java
@@ -18,177 +18,60 @@
 
 package bbcdabao.componentsbrz.websocketbrz.impl;
 
-import java.io.IOException;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.socket.PingMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import javax.validation.constraints.NotNull;
 
-import bbcdabao.componentsbrz.websocketbrz.api.ISessionSender;
+import bbcdabao.componentsbrz.websocketbrz.api.IGetMsgForSend;
 
 /**
  * Separate thread sending for WebSocketMessage module
  */
-public class SessionSender extends Thread implements ISessionSender {
-
-	/**
-	 * Send transfer interface
-	 */
-	private static interface ISend {
-		void send(WebSocketMessage<?> msg);
-	}
+public class SessionSender implements Runnable {
 
 	private final Logger logger = LoggerFactory.getLogger(SessionSender.class);
 
 	/**
-	 * Then WebSocketMessage list waiting for send
-	 */
-	private BlockingQueue<WebSocketMessage<?>> msgList = null;
-
-	/**
 	 * The unit of sending PING cycle is milliseconds, must not be null
 	 */
-	private WebSocketSession session = null;
+	private WebSocketSession session;
 
 	/**
-	 * Send completion notification interface, it can be null
+	 * Then WebSocketMessage get for send
 	 */
-	private IComplete complete = null;
+	private IGetMsgForSend getMsgForSend;
 
-	private long pingCyc = 0;
+	private AtomicLong timeSet;
 
-	private long pingFlg = 0;
-
-	private AtomicLong timeSet = null;
-
-	private WebSocketMessage<?> getMsg() {
-		WebSocketMessage<?> msg = null;
-		try {
-			msg = msgList.poll(1000, TimeUnit.MICROSECONDS);
-		} catch (InterruptedException e) {
-			logger.info("session id:{} sender is Interrupted", session.getId());
-		}
-		return msg;
-	}
-	
-	private void doSend(ISend send) {
-		try (WebSocketSession sessionClose = session) {
-			while (session.isOpen()) {
-				WebSocketMessage<?> msg = getMsg();
-				if (null != msg) {
-					send.send(msg);
-					timeSet.set(0);
-				} else {
-					checkAndSendPing();
-				}
-			}
-		} catch (Exception e) {
-			logger.error("session id:{} doSend Exception", session.getId(), e);
-		}
-	}
-
-	private void checkAndSendPing() {
-		long thisSet = timeSet.get();
-		if (0 == thisSet) {
-			// Just send the message and return directly without sending PING again.
-			pingFlg = 0;
-			return;
-		}
-		long thisNow = System.currentTimeMillis();
-		if (0 == pingFlg) {
-			pingFlg = thisNow;
-			return;
-		}
-		long thisSub = thisNow - pingFlg;
-		if (thisSub < pingCyc) {
-			return;
-		}
-		pingFlg = thisNow;
-		logger.info("session id:{} send ping >>>", session.getId());
-		try {
-			session.sendMessage(new PingMessage());
-		} catch(Exception e) {
-			logger.info("session id:{} checkAndSendPing Exception:", session.getId(), e);
-		}
-	}
-
-	public SessionSender(@NotNull(message = "session must not null") WebSocketSession session, @NotNull(message = "timeSet must not null") AtomicLong timeSet, IComplete complete, long pingCyc, int capacity) {
-		this.msgList = new LinkedBlockingQueue<>(capacity);
+	public SessionSender(@NotNull(message = "session must not null") WebSocketSession session,
+			@NotNull(message = "IGetMsgForSend must not null") IGetMsgForSend getMsgForSend,
+			@NotNull(message = "timeSet must not null") AtomicLong timeSet) {
 		this.session = session;
-		this.complete = complete;
+		this.getMsgForSend = getMsgForSend;
 		this.timeSet = timeSet;
-		this.pingCyc = pingCyc;
-		start();
-	}
-
-	@Override
-	public boolean postMsg(WebSocketMessage<?> msg, long timeout, TimeUnit unit) throws InterruptedException {
-		return msgList.offer(msg, timeout, unit);
-	}
-
-	@Override
-	public boolean postMsg(WebSocketMessage<?> msg) {
-		return msgList.offer(msg);
-	}
-
-	@Override
-	public boolean isOpen() {
-		return session.isOpen();
-	}
-
-	@Override
-	public String getSessionId() {
-		return session.getId();
-	}
-
-	@Override
-	public void close() throws IOException {
-		session.close();
-		interrupt();
 	}
 
 	@Override
 	public void run() {
-		logger.info("session id:{} sender begin to working...", session.getId());
-		if (complete != null) {
-			doSend(new ISend() {
-				@Override
-				public void send(WebSocketMessage<?> msg) {
-					Throwable exception = null;
-					boolean ok = false;
-					try {
-						session.sendMessage(msg);
-						ok = true;
-					} catch(Exception e) {
-						exception = e;
-					} finally {
-						try {
-							complete.onComplete(msg, ok, exception);
-						} catch (Exception e) {
-							logger.error("session id:{} onComplete Exception", session.getId(), e);
-						}
-					}
-				}
-			});
-		} else {
-			doSend(new ISend() {
-				@Override
-				public void send(WebSocketMessage<?> msg) {
+		try (WebSocketSession sessionClose = session) {
+			while (session.isOpen()) {
+				WebSocketMessage<?> msg = getMsgForSend.getMsg();
+				if (null != msg) {
 					try {
 						session.sendMessage(msg);
 					} catch(Exception e) {
 						logger.info("session id:{} doSend0 Exception:", session.getId(), e);
 					}
+					timeSet.set(0);
 				}
-			});
+			}
+		} catch (Exception e) {
+			logger.error("session id:{} doSend Exception", session.getId(), e);
 		}
 	}
 }
