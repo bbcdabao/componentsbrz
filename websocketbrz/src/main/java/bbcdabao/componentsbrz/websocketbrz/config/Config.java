a36245c3f6ab22aa2a76ad5c913355992eb00d4c
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-02 17:14:39 +0800

    Update Config.java

--- websocketbrz/src/main/java/bbcdabao/componentsbrz/websocketbrz/config/Config.java
+++ websocketbrz/src/main/java/bbcdabao/componentsbrz/websocketbrz/config/Config.java
@@ -28,16 +28,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import bbcdabao.componentsbrz.websocketbrz.impl.BrzHandshakeInterceptor;
 import bbcdabao.componentsbrz.websocketbrz.impl.BrzWebSocketServer;
@@ -46,16 +39,9 @@ import bbcdabao.componentsbrz.websocketbrz.impl.BrzWebSocketServer;
  * Start component configuration
  */
 @Configuration
-@EnableWebSocket
-public class Config implements WebSocketConfigurer {
+public class Config {
 
 	private final Logger logger = LoggerFactory.getLogger(Config.class);
-
-	@Value("${wscfg.paths:bbadabao}")
-	private String paths;
-
-	@Value("${wscfg.allowedOrigins:*}")
-	private String allowedOrigins;
 
 	@Value("${wscfg.isPartialMsg:true}")	
 	private boolean isPartialMsg;
@@ -69,17 +55,8 @@ public class Config implements WebSocketConfigurer {
 	@Value("${wscfg.maxSessions:20}")
 	private int maxSessions;
 
-	@Value("${wscfg.taskSchedulerSize:10}")
-	private int taskSchedulerSize;
-
-	@Value("${wscfg.pingCyc:10000}")
+	@Value("${wscfg.pingCyc:0}")
 	private long pingCyc;
-
-	@Autowired
-	private BrzWebSocketServer brzWebSocketServer;
-
-	@Autowired
-	private BrzHandshakeInterceptor brzHandshakeInterceptor;
 
 	/**
 	 * Thread pool config
@@ -100,15 +77,6 @@ public class Config implements WebSocketConfigurer {
     private String threadNamePrefix;
 
 	@Bean
-	@ConditionalOnProperty(prefix = "wscfg", name = "hadscheduler", havingValue = "true", matchIfMissing = false)
-	public TaskScheduler taskScheduler() {
-		ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-		taskScheduler.setPoolSize(taskSchedulerSize);
-		taskScheduler.initialize();
-		return taskScheduler;
-	}
-
-	@Bean
 	public BrzWebSocketServer brzWebSocketServer() {
 		return new BrzWebSocketServer(isPartialMsg, timeCyc, stepOut, maxSessions, pingCyc);
 	}
@@ -116,14 +84,6 @@ public class Config implements WebSocketConfigurer {
 	@Bean
 	public BrzHandshakeInterceptor brzHandshakeInterceptor() {
 		return new BrzHandshakeInterceptor();
-	}
-
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry
-		.addHandler(brzWebSocketServer, paths.split(","))
-		.addInterceptors(brzHandshakeInterceptor)
-		.setAllowedOrigins(allowedOrigins.split(","));
 	}
 
 	/**
