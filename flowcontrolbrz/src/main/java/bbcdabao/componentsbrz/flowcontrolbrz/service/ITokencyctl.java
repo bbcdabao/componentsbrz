582c099777d99ee4d94918a27df3d2f05026a9f2
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-02 11:39:26 +0800

    Update ITokencyctl.java

--- flowcontrolbrz/src/main/java/bbcdabao/componentsbrz/flowcontrolbrz/service/ITokencyctl.java
+++ flowcontrolbrz/src/main/java/bbcdabao/componentsbrz/flowcontrolbrz/service/ITokencyctl.java
@@ -1,9 +1,7 @@
 package bbcdabao.componentsbrz.flowcontrolbrz.service;
 
 /**
- * -第三方并发控制接口，用于实现令牌彤方式限流控制
- * @author bao
- *
+ * Tokencyctl
  */
 public interface ITokencyctl {
 
@@ -13,15 +11,7 @@ public interface ITokencyctl {
 		public int maxToken;
 	}
 
-	/**
-	 * -设置参数
-	 * @param tokenCfg
-	 */
 	void setTokenCfg(TokenCfg tokenCfg);
 
-	/**
-	 * -获取参数
-	 * @return
-	 */
 	TokenCfg getTokenCfg();
 }
