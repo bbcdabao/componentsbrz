05bac13c0c5d1313ea8d8ef8bcd49f356e84dbe4
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-02 17:15:32 +0800

    Create ConfigWebSocket.java

---
+++ websocketbrz/src/main/java/bbcdabao/componentsbrz/websocketbrz/config/ConfigWebSocket.java
@@ -0,0 +1,36 @@
+package bbcdabao.componentsbrz.websocketbrz.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import bbcdabao.componentsbrz.websocketbrz.impl.BrzHandshakeInterceptor;
+import bbcdabao.componentsbrz.websocketbrz.impl.BrzWebSocketServer;
+
+@Configuration
+@EnableWebSocket
+public class ConfigWebSocket implements WebSocketConfigurer {
+
+	@Value("${wscfg.paths:bbadabao}")
+	private String paths;
+
+	@Value("${wscfg.allowedOrigins:*}")
+	private String allowedOrigins;
+
+	@Autowired
+	private BrzWebSocketServer brzWebSocketServer;
+
+	@Autowired
+	private BrzHandshakeInterceptor brzHandshakeInterceptor;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry
+		.addHandler(brzWebSocketServer, paths.split(","))
+		.addInterceptors(brzHandshakeInterceptor)
+		.setAllowedOrigins(allowedOrigins.split(","));
+	}
+}
