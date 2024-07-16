feab7dbc8578b5c1e151814c5d0e30f48242bd71
bao <bao_ming_hui@126.com>
2024-07-17 00:16:00 +0800

    modedif api

--- terminalhub-ui/src/api/index.ts
+++ terminalhub-ui/src/api/index.ts
@@ -1,17 +1,17 @@
 import request from '../utils/request';
 
 /*
-url: 请求的服务器 URL。只需要在单独的请求中指定。
-method: 请求方法，默认是 GET。
-baseURL: 将 baseURL 和 url 拼接成完整的 URL，如果 url 是绝对 URL 则不会拼接。
-headers: 自定义的 HTTP 头。
-params: URL 参数，会拼接到 URL 后面。
-data: 请求体数据，只适用于 PUT、POST、PATCH 等方法。
-timeout: 指定请求的超时时间（毫秒）。
-withCredentials: 跨域请求时是否需要使用凭证。
-responseType: 期望的服务器响应的数据类型，常用的有 json、text、blob、arraybuffer。
-auth: HTTP 基本认证 { username: '...', password: '...' }。
-proxy: 代理配置 { host: '127.0.0.1', port: 9000 }。
+url: The requested server URL. Just need to be specified in a separate request.
+method: Request method, the default is GET.
+baseURL: Concatenate baseURL and url into a complete URL. If url is an absolute URL, it will not be concatenated.
+headers: Custom HTTP headers.
+params: URL parameters will be spliced ​​to the end of the URL.
+data: Seeking volume data is only applicable to PUT, POST, PATCH and other methods.
+timeout: Specifies the request timeout in milliseconds.
+withCredentials: Whether credentials are required for cross-domain requests.
+responseType: The expected data type of the server response. Commonly used ones include json, text, blob, and arraybuffer.
+auth: HTTP Basic Authentication { username: '...', password: '...' }.
+proxy: 
 */
 
 export const loginpost = (body : any = {}) => {
