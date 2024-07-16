0725365d7a2684d1530bde69ab064121e3ca6139
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-16 19:47:59 +0800

    Create LoginDto.java

---
+++ terminalhub/src/main/java/bbcdabao/componentsbrz/terminalhub/model/LoginDto.java
@@ -0,0 +1,26 @@
+package bbcdabao.componentsbrz.terminalhub.model;
+
+/**
+ * login message
+ */
+public class LoginDto {
+
+	private String username;
+	private String passwrod;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPasswrod() {
+        return passwrod;
+    }
+
+    public void setPasswrod(String passwrod) {
+        this.passwrod = passwrod;
+    }
+}
