34c4de591bdfffb7841327939bf6252abbaa3682
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-16 19:48:47 +0800

    Create LoginResponseDto.java

---
+++ terminalhub/src/main/java/bbcdabao/componentsbrz/terminalhub/model/LoginResponseDto.java
@@ -0,0 +1,14 @@
+package bbcdabao.componentsbrz.terminalhub.model;
+
+public class LoginResponseDto {
+
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
