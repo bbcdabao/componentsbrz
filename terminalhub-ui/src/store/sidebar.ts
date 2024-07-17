5538b69e0ea113c7c474e3d0a5edfbb1479e71b0
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-17 17:51:57 +0800

    Update sidebar.ts

--- terminalhub-ui/src/store/sidebar.ts
+++ terminalhub-ui/src/store/sidebar.ts
@@ -1,9 +1,11 @@
 import { defineStore } from 'pinia';
+import { Menus } from '@/types/menu';
 
 export const useSidebarStore = defineStore('sidebar', {
 	state: () => {
 		return {
 			collapse: false,
+			menuData: JSON.parse(localStorage.getItem('sidebar-items') || '[]') as Menu[],
 			bgColor: localStorage.getItem('sidebar-bg-color') || '#000000',
 			textColor: localStorage.getItem('sidebar-text-color') || '#bfcbd9'
 		};
