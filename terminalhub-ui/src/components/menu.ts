f39a227b369624005dc36d1e617853db5d764d18
bbcdabao <45411947+bbcdabao@users.noreply.github.com>
2024-07-17 17:50:53 +0800

    Update menu.ts

--- terminalhub-ui/src/components/menu.ts
+++ terminalhub-ui/src/components/menu.ts
@@ -2,55 +2,22 @@ import { Menus } from '@/types/menu';
 
 export const menuData: Menus[] = [
     {
-        title: '页面01',
-        index: '/page01',
-        icon: 'eleme-filled',
+        title: '添加终端配置',
+        index: '/page01dd',
+        icon: 'circle-plus-filled',
     },
     {
-        title: '菜单1',
+        title: '当前添加终端',
         index: '1',
-        icon: 'menu',
+        icon: 'eleme',
         children: [
-            {
-                index: '/page01',
-                title: '页面01',
-            },
-            {
-                index: '/page02',
-                title: '页面02',
-            },
-            {
-                index: '/page03',
-                title: '页面03',
-            },
-            {
-                index: '1-1',
-                title: '三级菜单',
-                children: [
-                    {
-                        index: '/page04',
-                        title: '页面04',
-                    },
-                    {
-                        index: '/page05',
-                        title: '页面05',
-                    },
-                    {
-                        index: '/page06',
-                        title: '页面06',
-                    },
-                ],
-            },
         ],
     },
     {
-        icon: 'brush',
-        index: '/theme',
-        title: '主题',
-    },
-    {
-        icon: 'guide',
-        index: '/icon',
-        title: '图标',
+        title: '后台管理终端',
+        index: '2',
+        icon: 'eleme-filled',
+        children: [
+        ],
     },
 ];
