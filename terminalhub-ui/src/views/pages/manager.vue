086dedf7e221fa0d841f7b71c81d00125fc72e76
bao <bao_ming_hui@126.com>
2024-08-28 20:43:23 +0800

    modify

--- terminalhub-ui/src/views/pages/manager.vue
+++ terminalhub-ui/src/views/pages/manager.vue
@@ -5,25 +5,19 @@
                 <template #header>
                     <div class="content-title">{{ $t('managerWelcome') }}</div>
                 </template>
-                <div class="manager-items">
-                    <div class="manager-item">
+                <div class="manager-form-items">
+                    <div class="manager-form-item">
                         <el-form :model="form" :rules="rules" ref="formRef" autocomplete="off">
-                            <el-form-item label="地址" prop="addr">
+                            <el-form-item :label="$t('addr')" prop="addr">
                                 <el-input v-model="form.addr"></el-input>
                             </el-form-item>
-                            <el-form-item label="用户" prop="user">
+                            <el-form-item :label="$t('user')" prop="user">
                                 <el-input v-model="form.user" autocomplete="new-username"></el-input>
                             </el-form-item>
-                            <el-form-item label="密码" prop="pass">
-                                <el-input v-model="form.pass" type="password" autocomplete="new-password"></el-input>
+                            <el-form-item :label="$t('pass')" prop="pass">
+                                <el-input v-model="form.pass" autocomplete="new-password"></el-input>
                             </el-form-item>
-                            <el-input
-                                type="textarea"
-                                :autosize="{ minRows: 2, maxRows: 4}"
-                                :placeholder="$t('pleaseEnterCmd')"
-                                v-model="runcmds">
-                            </el-input>
-                            <el-button style="margin-top: 10px;" type="primary" @click="submitForm">{{ $t('addTerminal') }}</el-button>
+                            <el-button style="margin-top: 0px;" type="primary" @click="submitForm">{{ $t('addTerminal') }}</el-button>
                         </el-form>
                     </div>
                 </div>
@@ -34,8 +28,8 @@
                 <template #header>
                     <div class="content-title">{{ $t('terminalManagement') }}</div>
                 </template>
-                <div class="manager-nodeitems">
-                    <div class="manager-nodeitem" v-for="(item, index) in sidebar.sshitems" :key="index">
+                <div class="manager-node-items">
+                    <div class="manager-node-item" v-for="(item, index) in sidebar.sshitems" :key="index">
                         <el-button style="font-size: 16px;" size="small" type="danger" @click="deleteItem(index)" icon="delete-filled" circle />
                         {{ item.addr }}
                     </div>
@@ -45,13 +39,15 @@
     </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useSidebarStore } from '@/store/sidebar';
 import { ElButton, ElForm, ElFormItem, ElInput, ElMessage, FormRules } from 'element-plus';
+import { Sshitem } from '@/types/sshitem';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const sidebar = useSidebarStore();
-
-const runcmds = ref("");
 
 const formRef = ref(null);
 const form = ref({
@@ -85,26 +81,20 @@ const submitForm = () => {
       sidebar.addSshitem({
         addr: form.value.addr,
         user: encodeURIComponent(form.value.user),
-        pass: encodeURIComponent(form.value.pass),
-        cmds: runcmds.value
-      }) 
+        pass: encodeURIComponent(form.value.pass)
+      })
+        ElMessage.success(t('addSuccess'))
     } else {
-      ElMessage.error('fail')
+        ElMessage.error(t('addFail'))
     }
   })
 };
 
 const deleteItem = (index: string | number) => {
     if (typeof index === 'string') {
-        sidebar.delSshitem(index);
-    }
-};
-
-const setItem = (index: string | number) => {
-    if (typeof index === 'string') {
         let sshitem = sidebar.sshitems[index];
-        form.value = sshitem;
-        runcmds.value = sshitem.cmds;
+        sidebar.delSshitem(index);
+        ElMessage.success(t('deleteTerminal') + sshitem.addr);
     }
 };
 
@@ -114,44 +104,40 @@ const setItem = (index: string | number) => {
     display: flex;
     flex-direction: column;
 }
-.logo {
-    width: 200px;
-    padding: 10px;
-    flex-shrink: 0;
-}
-.manager-items {
+.manager-form-items {
     display: flex;
     align-items: flex-start;
 }
-.manager-item {
+.manager-form-item {
     flex-grow: 1;
-    width: 400px;
-    padding: 10px;
     border-radius: 5px;
-    background-color: var(--sidebar-index-bg-color);
+    background-color: transparent;
 }
-.manager-nodeitems {
+.manager-node-items {
     display: flex;
-    align-items: flex-start;
     flex-wrap: wrap;
+    align-items: flex-start;
 }
-.manager-nodeitem {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+@keyframes expandWidth {
+    0% {
+        width: 0px;
+    }
+    100% {
+        width: 250px;
+    }
+}
+.manager-node-item {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
     padding: 4px;
-    color: var(--nodept-text-color);
-    width: 215x;
+    width: 250px;
     margin: 4px;
     border-radius: 5px;
     font-weight: bold;
     flex-shrink: 0;
+    color: var(--nodept-text-color);
     background-color: var(--nodept-bg-color);
-}
-.el-image {
-  margin-left: 20px;
-  width: 200px;
-  border-radius: 20px;
-  flex-shrink: 0;
+    animation: expandWidth 0.5s ease-in-out;
 }
 </style>
